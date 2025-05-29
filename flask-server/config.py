b79867e7a373c3215df09d64f11c09b82ccb9172
Leonardo Oliveira <143437480+Iamlnx@users.noreply.github.com>
2025-05-29 09:43:19 -0300

    Update config.py

--- flask-server/config.py
+++ flask-server/config.py
@@ -4,3 +4,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")  
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY")
+    SESSION_COOKIE_SAMESITE = "None"
+    SESSION_COOKIE_SECURE = True
